8b035e8ae63ccf790cdbc3c7151fc924fb23f154
Mate Balogh <b.mate11@gmail.com>
2017-10-29 19:50:49 +0100

    TimerEvents was implemented

---
+++ TimerEvents/TimerEvents/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TimerEvents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Tick += new EventHandler(t_Tick);
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 10;
+            if (progressBar1.Value >= 100) {
+                timer1.Stop();
+                textBox1.Show();
+                textBox1.Text = "Loading is done baby!";
+            }
+        }
+    }
+}
