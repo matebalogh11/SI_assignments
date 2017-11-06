59f7abdd09bf5bfbf303d2924245ce3ebf784601
Mate Balogh <b.mate11@gmail.com>
2017-11-06 16:11:59 +0100

    AdvancedExercise 1st task was implemented

---
+++ Exercise1Collections/SearchPattern/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SearchPattern
+{
+    class Program
+    {
+        static List<FileInfo> FoundFiles;
+
+        static void Main(string[] args)
+        {
+            string fileName = args[0];
+            string directoryName = args[1];
+            FoundFiles = new List<FileInfo>();
+
+            DirectoryInfo rootDir = new DirectoryInfo(directoryName);
+            if (!rootDir.Exists)
+            {
+                Console.WriteLine("The given directory does not exist.");
+                return;
+            }
+            RecursiveSearch(FoundFiles, fileName, rootDir);
+            Console.WriteLine($"Found {FoundFiles.Count} files");
+            FoundFiles.ForEach(x => Console.WriteLine(x.FullName));
+            Console.ReadKey();
+        }
+
+        static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
+        {
+            foreach (FileInfo file in currentDirectory.GetFiles())
+            {
+                if (file.Name == fileName)
+                {
+                    FoundFiles.Add(file);
+                }
+            }
+            foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, fileName, dir);
+            }
+        }
+    }
+}
