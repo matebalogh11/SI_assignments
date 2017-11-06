b12cf59e590eefb5beaa8e2b53a0d87446356484
Mate Balogh <b.mate11@gmail.com>
2017-11-06 18:29:49 +0100

    Exercise 2 and 3 were implemented as well

--- Exercise1Collections/SearchPattern/Program.cs
+++ Exercise1Collections/SearchPattern/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 namespace SearchPattern
 {
     class Program
     {
         static List<FileInfo> FoundFiles;
+        static List<FileSystemWatcher> watchers;
+        static List<DirectoryInfo> archiveDirs;
 
         static void Main(string[] args)
         {
             string fileName = args[0];
             string directoryName = args[1];
+
             FoundFiles = new List<FileInfo>();
+            watchers = new List<FileSystemWatcher>();
 
             DirectoryInfo rootDir = new DirectoryInfo(directoryName);
             if (!rootDir.Exists)
@@ -23,7 +29,24 @@ namespace SearchPattern
             RecursiveSearch(FoundFiles, fileName, rootDir);
             Console.WriteLine($"Found {FoundFiles.Count} files");
             FoundFiles.ForEach(x => Console.WriteLine(x.FullName));
-            Console.ReadKey();
+
+            foreach (FileInfo file in FoundFiles)
+            {
+                FileSystemWatcher newWatcher = new FileSystemWatcher(file.DirectoryName, file.Name);
+                newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
+                newWatcher.Deleted += new FileSystemEventHandler(WatcherDeleted);
+                newWatcher.EnableRaisingEvents = true;
+                watchers.Add(newWatcher);
+            }
+
+            archiveDirs = new List<DirectoryInfo>();
+            for (int i = 0; i < FoundFiles.Count; i++)
+            {
+                archiveDirs.Add(Directory.CreateDirectory("archive" + i.ToString()));
+            }
+
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
         }
 
         static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
@@ -39,6 +62,36 @@ namespace SearchPattern
             {
                 RecursiveSearch(foundFiles, fileName, dir);
             }
+        }
+        static void WatcherChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                Console.WriteLine("{0} has been changed!", e.FullPath);
+                FileSystemWatcher senderWatcher = (FileSystemWatcher)sender;
+                int index = watchers.IndexOf(senderWatcher, 0);
+                ArchiveFile(archiveDirs[index], FoundFiles[index]);
+            }
+        }
+        static void WatcherDeleted(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Deleted)
+            {
+                Console.WriteLine("{0} has been deleted!", e.FullPath);
+                FileSystemWatcher check = (FileSystemWatcher)sender;
+                watchers.Remove(check);
+            }
+        }
+        static void ArchiveFile(DirectoryInfo archiveDir, FileInfo fileToArchive)
+        {
+            FileStream input = fileToArchive.OpenRead();
+            FileStream output = File.Create(archiveDir.FullName + @"\" + fileToArchive.Name + ".gz");
+            GZipStream Compressor = new GZipStream(output, CompressionMode.Compress);
+            input.CopyTo(Compressor);
+
+            Compressor.Close();
+            input.Close();
+            output.Close();
         }
     }
 }
