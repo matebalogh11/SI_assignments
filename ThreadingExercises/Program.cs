2ce27a651df5e03074b3ccdcc871796095a0276c
Mate Balogh <b.mate11@gmail.com>
2017-11-21 10:07:32 +0100

    Threading ex1 was implemented

---
+++ ThreadingExercises/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart myStart = new ThreadStart(Counting);
+            Thread t1 = new Thread(myStart);
+            Thread t2 = new Thread(myStart);
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.ReadKey();
+        }
+
+        static void Counting()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Current count: {i} thread id: {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
