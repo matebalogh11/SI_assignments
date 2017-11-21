30c14575c828488dcd04c4ab2bc7364f63940b65
Mate Balogh <b.mate11@gmail.com>
2017-11-21 10:36:55 +0100

    Exercise 3 was implemented

---
+++ ThreadingExercises/ThreadPoolDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback myCall = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(myCall, "hello");
+            ThreadPool.QueueUserWorkItem(myCall, "hi");
+            ThreadPool.QueueUserWorkItem(myCall, "hey");
+            ThreadPool.QueueUserWorkItem(myCall, "yo");
+            Console.ReadKey();
+        }
+
+        static void ShowMyText(object state)
+        {
+            string myString = "State " + state as string;
+            myString += " Thread " + Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(myString);
+        }
+    }
+}
