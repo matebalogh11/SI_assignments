9d317e149b1d16c79547164229221a32f2851ea0
Mate Balogh <b.mate11@gmail.com>
2017-11-23 18:35:53 +0100

    Ex3 was implemented

---
+++ RegularExpressions/FileEncoding/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Text;
+
+namespace FileEncoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"C:\Users\bmate\Documents\Overwatch\Settings\Settings_v0.ini"))
+            using (StreamWriter writer = new StreamWriter("boot-utf47.txt", false, Encoding.UTF7))
+            writer.WriteLine(reader.ReadToEnd());
+        }
+    }
+}
