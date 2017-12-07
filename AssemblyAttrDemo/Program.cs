d964b7ae71ec3e48972ced4267dbedb7c13bc74d
Mate Balogh <b.mate11@gmail.com>
2017-12-07 10:32:02 +0100

    Ex2 was implemented

---
+++ AssemblyAttrDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type assemblyDesc = typeof(AssemblyDescriptionAttribute);
+            object[] obj =  assembly.GetCustomAttributes(assemblyDesc, false);
+            if (obj.Length > 0)
+            {
+                AssemblyDescriptionAttribute attr;
+                attr = obj[0] as AssemblyDescriptionAttribute;
+                Console.WriteLine(attr.Description);
+            }
+            Console.ReadKey();
+        }
+    }
+}
