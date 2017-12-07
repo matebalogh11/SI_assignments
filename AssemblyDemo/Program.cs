33dd1d317f93f456b49eb8f7b30a7e49c8e761e5
Mate Balogh <b.mate11@gmail.com>
2017-12-07 09:32:37 +0100

    AssemblyDemo ex1 was implemented

---
+++ AssemblyDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string longName = "system, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089";
+            Assembly assembly = Assembly.Load(longName);
+            ShowAssembly(assembly);
+
+            Assembly ourAssembly = Assembly.GetExecutingAssembly();
+            ShowAssembly(ourAssembly);
+
+            Console.ReadKey();
+        }
+
+        static void ShowAssembly(Assembly assembly)
+        {
+            Console.WriteLine(assembly.FullName);
+            Console.WriteLine(assembly.GlobalAssemblyCache);
+            Console.WriteLine(assembly.Location);
+            Console.WriteLine(assembly.ImageRuntimeVersion);
+            Console.WriteLine(Environment.NewLine +  "Modules: ");
+            foreach (Module module in assembly.GetModules())
+            {
+                Console.WriteLine(module.Name);
+            }
+            Console.WriteLine(Environment.NewLine);
+        }
+    }
+}
