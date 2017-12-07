f813cf357449b3507afd7b07e49c8feb68c6f818
Mate Balogh <b.mate11@gmail.com>
2017-12-07 14:34:01 +0100

    Ex5 was implemented

---
+++ DemoDynamic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Reflection;
+
+namespace DemoDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyName myName = new AssemblyName();
+            myName.Name = "Mynewproject";
+            myName.Version = new Version("1.0.0.0");
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            AssemblyBuilder myAsBuilder =  currentDomain.DefineDynamicAssembly(myName, AssemblyBuilderAccess.ReflectionOnly);
+            ModuleBuilder myModBuilder = myAsBuilder.DefineDynamicModule("CodeModule", "DemoAssembly.dll");
+            TypeBuilder myTypeBuilder = myModBuilder.DefineType("myType", TypeAttributes.Public);
+            Type finalObj = myTypeBuilder.CreateType();
+            foreach (MemberInfo info in finalObj.GetMembers())
+            {
+                Console.WriteLine($"Member type: {info.MemberType} and name: {info.Name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
