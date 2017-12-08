8602de82361f550dd49020004201a9815c1b4450
Mate Balogh <b.mate11@gmail.com>
2017-12-08 09:34:50 +0100

    LogEx1 was implemented - security error

---
+++ InstrumentationEx1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace InstrumentationEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!EventLog.SourceExists("Demo", "DESKTOP-27ATJO4"))
+            {
+                EventSourceCreationData eventData = new EventSourceCreationData("Demo", "Application");
+                eventData.MachineName = "DESKTOP-27ATJO4";
+                EventLog.CreateEventSource(eventData);
+            }
+            EventLog logDemo = new EventLog("Demo", "Application", "DESKTOP-27ATJO4");
+            logDemo.WriteEntry("Event written to application log", EventLogEntryType.Information, 234, Convert.ToInt16(3));
+        }
+    }
+}
