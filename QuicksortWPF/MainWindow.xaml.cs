59c07f2bcc2e950c7a7bab78808298a149e8e5fa
Mate Balogh <b.mate11@gmail.com>
2018-01-07 13:13:34 +0100

    Quicksort was implemented

---
+++ QuicksortWPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using QuickSortWPF;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QuicksortWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainForm : Window
+    {
+        int[] testArray = new int[] { 2, 6, 9, 3, 12, 4, 1 };
+
+        public MainForm()
+        {
+            InitializeComponent();
+            PopulateList("Old", this.oldList);
+        }
+
+        public void PopulateList(string listType, ListView currentList)
+        {
+            var gridView = new GridView();
+            currentList.View = gridView;
+
+            gridView.Columns.Add(new GridViewColumn { Header = listType});
+            foreach (int item in testArray)
+            {
+                currentList.Items.Add(item);
+            }
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            SortEngine.QuickSort(testArray);
+            PopulateList("New", this.newList);
+        }
+    }
+}
---
+++ SortAlgo/SortEngine.cs
@@ -0,0 +1,43 @@
+﻿namespace QuickSortWPF
+{
+    public class SortEngine
+    {
+        public static void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        static void QuickSort(int[] array, int index, int pivotIndex)
+        {
+            if (index >= pivotIndex) return;
+            index = Partitioning(array, index, pivotIndex);
+            QuickSort(array, index, pivotIndex);
+            QuickSort(array, 0, index - 1);
+        }
+
+        static int Partitioning(int[] array, int index, int pivotIndex)
+        {
+            int current = index;
+            while (index <= pivotIndex && current < pivotIndex)
+            {
+                while(array[current] > array[pivotIndex])
+                {
+                    current++;
+                }
+                if (index <= pivotIndex && array[index] > array[current])
+                {
+                    Swap(array, index, current);
+                    index++;
+                } else { current++; index++; }
+            }
+            return index;
+        }
+
+        static void Swap(int[] array, int left, int rigth)
+        {
+            int temp = array[left];
+            array[left] = array[rigth];
+            array[rigth] = temp;
+        }
+    }
+}
